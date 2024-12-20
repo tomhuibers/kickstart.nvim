71b227237bdd3d6bbe35a144cd13383ec1143305
Tom Huibers <tom@verticai.nl>
2024-12-20 13:53:38 +0100

    add tmux-navigator

--- lua/tom/plugins/oil.lua
+++ lua/tom/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true, -- Show dotfiles and hidden files
+    },
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
